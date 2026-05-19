ab91ee8cfbd77aba3dcf6f61be1a8c3a7dfde02d
polariskim4 <polariskim4@gmail.com>
2026-05-19 15:21:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -47,10 +47,11 @@ apps = [
     {"name": "5. 미국주식 MDD 확인", "url": "https://find-mdd-3qryxn4gmeexn5tnbgjjhx.streamlit.app/", "desc": "미국 개별주 및 ETF 과거 MDD 확인"},
     {"name": "6. 미국주식 재무 비교", "url": "https://stock-dashboard-tle5fvdmqujrqrvrqdb8c3.streamlit.app/", "desc": "미국 개별주 vs 벤치마크 종목 주요 재무지표 비교"},
     {"name": "7. 레버리지 시뮬레이터", "url": "https://leverage-visualizer-od6veunxjczztleqmnfc5k.streamlit.app/", "desc": "가상 2배 ETF와 벤치마크 간 수익률 및 MDD 비교"},
-    {"name": "8. 한국주식 재무 비교", "url": "https://k-stock-uki3zdrrysorupc3qd5w7k.streamlit.app/", "desc": "한국 개별주 vs 벤치마크 종목 주요 재무지표 비교"},
-    {"name": "9. 국내외 수익률 TOP 10", "url": "https://top-stock-bye8ingxzdftv9nlkc6cpr.streamlit.app/", "desc": "미국 및 한국 주식 수익률 상위 10종목 실시간 순위"},
-    {"name": "10. 미국 주식 원화 계산기", "url": "https://krw-amount-jpbvqirdphbhgwkwghsgiv.streamlit.app/", "desc": "미국 주식 매수시 종목, 수량 입력시 필요한 원화 금액 자동 계산"},
-    {"name": "11. 은퇴 계산기", "url": "https://retirement-calculator-cxvfymmsjzvyicxm6yktvs.streamlit.app/", "desc": "노후 준비를 위한 자금 시뮬레이션"}
+    {"name": "8. 미국 주식 수익률", "url": "https://performance-evr93cdbzj6bznha8nbmby.streamlit.app/", "desc": "미국 주식 과거 수익률 및 올해 YTD 수익률, 한국 종목도 조회 가능"},
+    {"name": "9. 한국주식 재무 비교", "url": "https://k-stock-uki3zdrrysorupc3qd5w7k.streamlit.app/", "desc": "한국 개별주 vs 벤치마크 종목 주요 재무지표 비교"},
+    {"name": "10. 국내외 수익률 TOP 10", "url": "https://top-stock-bye8ingxzdftv9nlkc6cpr.streamlit.app/", "desc": "미국 및 한국 주식 수익률 상위 10종목 실시간 순위"},
+    {"name": "11. 미국 주식 원화 계산기", "url": "https://krw-amount-jpbvqirdphbhgwkwghsgiv.streamlit.app/", "desc": "미국 주식 매수시 종목, 수량 입력시 필요한 원화 금액 자동 계산"},
+    {"name": "12. 은퇴 계산기", "url": "https://retirement-calculator-cxvfymmsjzvyicxm6yktvs.streamlit.app/", "desc": "노후 준비를 위한 자금 시뮬레이션"}
 ]
 
 # 앱 리스트를 순회하며 카드 생성
